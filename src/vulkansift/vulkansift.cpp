36e1c4e91182aff98ed60d6b8294dca2ed33ae55
Mael Aubert <mael.aub@gmail.com>
2020-12-15 09:43:30 +0100

    Setup CMake project structure

---
+++ src/vulkansift/vulkansift.cpp
@@ -0,0 +1,1 @@
+#include "vulkansift/vulkansift.h"
---
+++ src/vulkansift/vulkansift.h
@@ -0,0 +1,16 @@
+#ifndef VULKAN_SIFT_H
+#define VULKAN_SIFT_H
+
+struct VulkanSiftFeature
+{
+  int x;
+  int y;
+};
+
+class VulkanSiftDetector
+{
+  public:
+  VulkanSiftDetector();
+};
+
+#endif // VULKAN_SIFT_H
